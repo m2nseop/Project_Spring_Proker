e78037b01a0c85117e482069b5435f91be8342b0
kafka <jjk5158@naver.com>
2022-07-25 23:20:19 +0900

    2022/07/25

--- Frontend/js/cp_unauthorized.js
+++ Frontend/js/cp_unauthorized.js
@@ -1,9 +1,11 @@
-$(function(){
+/* 사이드바 기능 함수 */
+
+$(function () {
 
   var $s_button = $('.sidemenu_button');
   var $sidebar = $('#sidebar');
 
-  $s_button.click(function(){
+  $s_button.click(function () {
     //alert("클릭 확인 ");
 
     //$sidebar.css("background-color", "#ff0");
@@ -16,44 +18,75 @@ $(function(){
     // open 클래스가 없으면 추가하고, 있으면 빼버립니다. ( 작동확인 완료 )
 
     if ($sidebar.hasClass('open')) {
-      $sidebar.stop(true).animate({ left: '0px' }, duration);
+      $sidebar.stop(true).animate({ left: '0%' }, duration);
     } else {
-      $sidebar.stop(true).animate({ left: '-300px' }, duration);
+      $sidebar.stop(true).animate({ left: '-10%' }, duration);
     };
 
 
-      /*
 
-
-    $(function () {
-      var duration = 300;
-
-      var $side = $('#sidebar');
-      var $s_button = $('#sidemenu_button');
-
-      $s_button.click(function()){
-
-      });
-
-
-      var $sidebt = $side.find('#sidemenu_button').on('click', function () {
-        $side.toggleClass('open');
-
-        if ($side.hasClass('open')) {
-          $side.stop(true).animate({ left: '0px' }, duration);
-        } else {
-          $side.stop(true).animate({ left: '-300px' }, duration);
-          $sidebt.find('span').text('OPEN');
-        };
-      });
-    });
-
-    */
-
-
-    
-
-
-    
   })
+});
+
+
+/* 이메일 팝업 열기 함수 */
+
+$(function () {
+  //----- OPEN
+  $('[data-popup-open]').on('click', function (e) {
+    var targeted_popup_class = jQuery(this).attr('data-popup-open');
+    $('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
+
+    e.preventDefault();
+  });
+
+  //----- CLOSE
+  $('[data-popup-close]').on('click', function (e) {
+    var targeted_popup_class = jQuery(this).attr('data-popup-close');
+    $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
+
+    e.preventDefault();
+  });
+});
+
+/* 알림 슬라이드 다운 함수 */
+
+
+$(function () {
+  //----- OPEN
+
+  var $n_button = $('.notice_icon');
+  var $slidedown = $('.slide-down');
+
+
+  $n_button.click(function () {
+    //alert("클릭 확인 ");
+
+    //$slidedown.css("background-color", "#ff0");
+    // 버튼 클릭시 sidebar 색 변경 (연결확인 )
+
+    var duration = 300;
+
+    $slidedown.toggleClass('open');
+
+    // open 클래스가 없으면 추가하고, 있으면 빼버립니다. ( 작동확인 완료 )
+
+    if ($slidedown.hasClass('open')) {
+      $slidedown.slideDown(duration);
+    } else {
+      $slidedown.slideUp(duration);
+    };
+
+
+
+  })
+
+});
+
+/* id 표시 관련 함수들 */
+
+$(function () {
+
+  
+
 });
