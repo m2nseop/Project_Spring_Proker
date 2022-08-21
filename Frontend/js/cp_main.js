3b1c1f12a205372dde72f0015dc315a86a886c44
kafka <jjk5158@naver.com>
2022-08-21 19:06:40 +0900

    2022/08/21

--- Frontend/js/cp_main.js
+++ Frontend/js/cp_main.js
@@ -435,7 +435,7 @@ $(function () {
 
     // alert("클릭 확인 ");
 
-    // 버튼 클릭시 연결된 갤러이 영역 색상 변경 (연결 확인 )
+    // 버튼 클릭시 연결된 갤러라 영역 색상 변경 (연결 확인 )
 
 
     // $gallery_loading_area.css("background-color", "#ff0");
@@ -674,147 +674,139 @@ $(function () {
 
 /* 버튼 hover 효과 모음   */
 
-$(function () {
+// $(function () {
 
-  var $home_button = $('.home_button');
-  var $sidemenu_button = $('.sidemenu_button');
-  var $email_icon = $('.email_icon');
-  var $notice_icon = $('.notice_icon');
-  var $change_gallery = $('.change_gallery');
-  var $change_list = $('.change_list');
+//   var $home_button = $('.home_button');
+//   var $sidemenu_button = $('.sidemenu_button');
+//   var $email_icon = $('.email_icon');
+//   var $notice_icon = $('.notice_icon');
+//   var $change_gallery = $('.change_gallery');
+//   var $change_list = $('.change_list');
 
 
 
-  $home_button.hover(function(){
+//   $home_button.hover(function(){
 
-    // alert("오버 효과 입니다. ");
+//     // alert("오버 효과 입니다. ");
 
 
-    $home_button.removeClass('icon_white');
-    $home_button.addClass('icon_blue');
+//     $home_button.removeClass('icon_white');
+//     $home_button.addClass('icon_blue');
 
 
-  }, function(){
+//   }, function(){
 
-    // alert("오버 효과에서 벗어납니다.");
+//     // alert("오버 효과에서 벗어납니다.");
 
 
-    $home_button.removeClass('icon_blue');
-    $home_button.addClass('icon_white');
+//     $home_button.removeClass('icon_blue');
+//     $home_button.addClass('icon_white');
 
 
-  });
+//   });
 
-  $sidemenu_button.hover(function(){
+//   $sidemenu_button.hover(function(){
 
-    // alert("오버 효과 입니다. ");
+//     // alert("오버 효과 입니다. ");
 
 
-    $sidemenu_button.removeClass('icon_white');
-    $sidemenu_button.addClass('icon_blue');
+//     $sidemenu_button.removeClass('icon_white');
+//     $sidemenu_button.addClass('icon_blue');
 
 
-  }, function(){
+//   }, function(){
 
-    // alert("오버 효과에서 벗어납니다.");
+//     // alert("오버 효과에서 벗어납니다.");
 
 
-    $sidemenu_button.removeClass('icon_blue');
-    $sidemenu_button.addClass('icon_white');
+//     $sidemenu_button.removeClass('icon_blue');
+//     $sidemenu_button.addClass('icon_white');
 
 
-  });
+//   });
 
-  $email_icon.hover(function(){
+//   $email_icon.hover(function(){
 
-    // alert("오버 효과 입니다. ");
+//     // alert("오버 효과 입니다. ");
 
 
-    $email_icon.removeClass('icon_white');
-    $email_icon.addClass('icon_blue');
+//     $email_icon.removeClass('icon_white');
+//     $email_icon.addClass('icon_blue');
 
 
-  }, function(){
+//   }, function(){
 
-    // alert("오버 효과에서 벗어납니다.");
+//     // alert("오버 효과에서 벗어납니다.");
 
 
-    $email_icon.removeClass('icon_blue');
-    $email_icon.addClass('icon_white');
+//     $email_icon.removeClass('icon_blue');
+//     $email_icon.addClass('icon_white');
 
 
-  });
+//   });
 
-  $notice_icon.hover(function(){
+//   $notice_icon.hover(function(){
 
-    // alert("오버 효과 입니다. ");
+//     // alert("오버 효과 입니다. ");
 
 
-    $notice_icon.removeClass('icon_white');
-    $notice_icon.addClass('icon_blue');
+//     $notice_icon.removeClass('icon_white');
+//     $notice_icon.addClass('icon_blue');
 
 
-  }, function(){
+//   }, function(){
 
-    // alert("오버 효과에서 벗어납니다.");
+//     // alert("오버 효과에서 벗어납니다.");
 
 
-    $notice_icon.removeClass('icon_blue');
-    $notice_icon.addClass('icon_white');
+//     $notice_icon.removeClass('icon_blue');
+//     $notice_icon.addClass('icon_white');
 
 
-  });
+//   });
 
-  $change_gallery.hover(function(){
+//   $change_gallery.hover(function(){
 
-    // alert("오버 효과 입니다. ");
+//     // alert("오버 효과 입니다. ");
 
 
-    $change_gallery.removeClass('icon_black');
-    $change_gallery.addClass('icon_blue');
+//     $change_gallery.removeClass('icon_black');
+//     $change_gallery.addClass('icon_blue');
 
 
-  }, function(){
+//   }, function(){
 
-    // alert("오버 효과에서 벗어납니다.");
+//     // alert("오버 효과에서 벗어납니다.");
 
 
-    $change_gallery.removeClass('icon_blue');
-    $change_gallery.addClass('icon_black');
+//     $change_gallery.removeClass('icon_blue');
+//     $change_gallery.addClass('icon_black');
 
 
-  });
+//   });
 
-  $change_list.hover(function(){
+//   $change_list.hover(function(){
 
-    // alert("오버 효과 입니다. ");
+//     // alert("오버 효과 입니다. ");
 
 
-    $change_list.removeClass('icon_black');
-    $change_list.addClass('icon_blue');
+//     $change_list.removeClass('icon_black');
+//     $change_list.addClass('icon_blue');
 
 
-  }, function(){
+//   }, function(){
 
-    // alert("오버 효과에서 벗어납니다.");
+//     // alert("오버 효과에서 벗어납니다.");
 
 
-    $change_list.removeClass('icon_blue');
-    $change_list.addClass('icon_black');
+//     $change_list.removeClass('icon_blue');
+//     $change_list.addClass('icon_black');
 
 
-  });
+//   });
 
 
 
 
 
-
-
-
-
-
-
-
-
-});
+// });
